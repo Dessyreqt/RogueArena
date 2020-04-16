c2c2b7aa38da55fc6782c5f0e9d66ea411283062
David Carroll <carroll.david.s@gmail.com>
2020-04-16 08:26:05 -0500

    Add test project

---
+++ RogueArena.Tests/TestingConvention.cs
@@ -0,0 +1,24 @@
+﻿namespace RogueArena.Tests
+{
+    using Fixie;
+
+    public class TestingConvention : Discovery, Execution
+    {
+        public TestingConvention()
+        {
+            Classes.Where(x => x.Name.StartsWith("When"));
+        }
+
+        public void Execute(TestClass testClass)
+        {
+            var instance = testClass.Construct();
+
+            testClass.RunCases(@case =>
+            {
+                @case.Execute(instance);
+            });
+
+            instance.Dispose();
+        }
+    }
+}
